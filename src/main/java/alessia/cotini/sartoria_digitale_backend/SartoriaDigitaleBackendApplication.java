e918f7a59c124fa1c6210d4540d36f607bac0b4e
AlessiaCotini <alessia.cotini@icloud.com>
2026-07-28 23:19:24 +0200

    Initial commit

---
+++ src/main/java/alessia/cotini/sartoria_digitale_backend/SartoriaDigitaleBackendApplication.java
@@ -0,0 +1,13 @@
+package alessia.cotini.sartoria_digitale_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SartoriaDigitaleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SartoriaDigitaleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alessia/cotini/sartoria_digitale_backend/SartoriaDigitaleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package alessia.cotini.sartoria_digitale_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SartoriaDigitaleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
